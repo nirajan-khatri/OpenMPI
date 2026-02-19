518d878b2989011f0fd073baa6b8b188ef3d8044
nirajan-khatri <nirajan.kh.dev@gmail.com>
2026-02-19 14:01:28 +0100

    add test job

---
+++ test_matmul.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+####### Mail Notify / Job Name / Comment #######
+#SBATCH --job-name="matmul_test"
+
+####### Partition #######
+#SBATCH --partition=all
+
+####### Ressources #######
+#SBATCH --time=0-00:02:00
+
+####### Node Info #######
+#SBATCH --exclusive
+#SBATCH --nodes=1
+
+####### Output #######
+#SBATCH --output=%x.out.%j
+#SBATCH --error=%x.err.%j
+
+module load gcc/14.3.0 openmpi
+make clean && make
+
+echo "=== Test 1: verbose, 4 procs ==="
+srun --ntasks=4 ./matmul 4 42 1
+
+echo ""
+echo "=== Test 2: non-verbose, 4 procs ==="
+srun --ntasks=4 ./matmul 4 42 0
+
+echo ""
+echo "=== Test 3: verbose, 1 proc ==="
+srun --ntasks=1 ./matmul 4 42 1
